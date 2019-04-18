d2fe4ba51188ba8e396133d3c35edaf575b5e96e
Robin Unger <robin.e.unger@gmail.com>
2019-04-18 18:13:25 -0500

    Refactor, remove unnecessary code

--- lib/chessboard.rb
+++ lib/chessboard.rb
@@ -5,6 +5,33 @@ class ChessBoard < Graph
     coordinates.each {|n| return false unless n.between?(0,7)}
     true
   end
+
+  def build(start_position, end_position)
+    queue = [Vertex.new(start_position)]
+    visited = []
+    knight = Knight.new
+    until queue.empty?
+      current_vertex = queue.pop
+
+      next if visited.include?(current_vertex.value)
+      knight.routes.each do |y,x|
+        possible_move = [current_vertex.value[0] + y, current_vertex.value[1] + x]
+        if valid_move(possible_move)
+          # keep track of the new vertex's parent for backtracking
+          temp = Vertex.new(possible_move, current_vertex)
+          current_vertex.add_edge(temp)
+          queue.unshift(temp)
+        end
+      end
+
+      add_vertex(current_vertex)
+      visited << current_vertex.value
+      break if current_vertex.value == end_position
+    end
+
+    self
+  end
+
 end
 
 class ChessPiece
--- lib/graph.rb
+++ lib/graph.rb
@@ -25,32 +25,6 @@ class Graph
     @vertices << vertex
   end
 
-  def build(start_position, end_position)
-    queue = [Vertex.new(start_position)]
-    visited = []
-    knight = Knight.new
-    until queue.empty?
-      current_vertex = queue.pop
-
-      next if visited.include?(current_vertex.value)
-      knight.routes.each do |y,x|
-        possible_move = [current_vertex.value[0] + y, current_vertex.value[1] + x]
-        if valid_move(possible_move)
-          # keep track of the new vertex's parent for backtracking
-          temp = Vertex.new(possible_move, current_vertex)
-          current_vertex.add_edge(temp)
-          queue.unshift(temp)
-        end
-      end
-
-      add_vertex(current_vertex)
-      visited << current_vertex.value
-      break if current_vertex.value == end_position
-    end
-
-    self
-  end
-
   # find shortest path using breadth first search
   def find_shortest_path(graph, end_position)
     queue = [graph.root]
